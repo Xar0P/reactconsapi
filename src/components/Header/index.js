64e097d2afd7a9a4648f14148bcaad49f6b45130
Xar0P <pietroricardocres@hotmail.com>
2021-12-12 22:47:35 -0300

    config routes

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,25 +1,19 @@
 import { FaHome, FaSignInAlt, FaUserAlt } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
-import { useSelector } from 'react-redux';
 import { Nav } from './styled';
 
 export default function Header() {
-  const botaoClicado = useSelector(
-    (state) => state.exampleReducer.botaoClicado
-  );
-
   return (
     <Nav>
       <Link to="/">
         <FaHome size={24} />
       </Link>
-      <Link to="/login">
+      <Link to="/register">
         <FaUserAlt size={24} />
       </Link>
-      <Link to="/sdsdsds">
+      <Link to="/login">
         <FaSignInAlt size={24} />
       </Link>
-      {botaoClicado ? 'Clicado' : 'Não clicado'}
     </Nav>
   );
 }
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,15 +1,30 @@
 import { Route, Routes as Switch } from 'react-router-dom';
 
-// import PrivateRoute from './PrivateRoute';
+import PrivateRoute from './PrivateRoute';
+import Student from '../pages/Student';
+import Students from '../pages/Students';
+import Photos from '../pages/Photos';
+import Register from '../pages/Register';
 import Login from '../pages/Login';
 import Page404 from '../pages/Page404/index';
 
 export default function Routes() {
   return (
     <Switch>
-      {/* <Route exact path="/" element={<PrivateRoute />}> */}
-      <Route exact path="/" element={<Login />} />
-      {/* </Route> */}
+      <Route exact path="/" element={<Students />} />
+
+      <Route exact path="/aluno/:id/edit" element={<PrivateRoute />}>
+        <Route exact path="/aluno/:id/edit" element={<Student />} />
+      </Route>
+      <Route exact path="/aluno/" element={<PrivateRoute />}>
+        <Route exact path="/aluno/" element={<Student />} />
+      </Route>
+      <Route exact path="/fotos/:id" element={<PrivateRoute />}>
+        <Route exact path="/fotos/:id" element={<Photos />} />
+      </Route>
+
+      <Route exact path="/login/" element={<Login />} />
+      <Route exact path="/register/" element={<Register />} />
       <Route path="*" element={<Page404 />} />
     </Switch>
   );
